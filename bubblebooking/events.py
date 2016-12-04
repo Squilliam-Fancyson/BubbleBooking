f32b2eb23aaeff901da46122b8140bee80eedfd7
ben <benkidd@live.com>
2016-12-04 12:52:36 -0800

    created models

---
+++ bubblebooking/events.py
@@ -0,0 +1,20 @@
+
+
+class Events(self. eventId):
+
+    self.eventId = eventId
+
+
+
+    #create and cancel events
+    def createEvent(self):
+        #todo
+
+    def cancelEvent(self, event_id):
+        #todo
+
+    #rsvp to existing event
+    def RSVP(self, event_id):
+        #todo
+
+
