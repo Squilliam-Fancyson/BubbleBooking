c2e25875d336c677fbd0fa400172088cffe11e59
Danny Thompson <dlt0012@auburn.edu>
2016-12-06 04:33:20 +0000

    Structured controllers

---
+++ bubblebooking/controllers/__init__.py
@@ -0,0 +1,10 @@
+"""Controllers for the various data models in the bubblebooking project.
+
+Classes:
+    PermissionController: Controls the permissions of user accounts.
+    
+    RoomController: Controls room booking requests.
+    
+    InvitationController: Controls delivery and reception of event invitations.
+    
+ """
